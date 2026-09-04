41b4b2ce745aec4803af181a8194c3a5e27cf90b
imashwaaa <imashwaaa@gmail.com>
2026-09-04 21:31:31 +0530

    Add first java code

--- Basics/Hello.java
+++ Basics/Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         
-        System.out.println("Siuuuu");
+        System.out.println("Hello Everyone");
+        System.out.println("This is my Java repo");
+        System.out.println("In this repo I will have all my code I write to learn Java.");
+        System.out.println("Also I will have dedicated folders for each of levels and for the projects too.");
+        System.out.println("See you in another file, siuuuu");
+
     }
 }
