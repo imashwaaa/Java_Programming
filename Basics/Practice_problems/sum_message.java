ba851cd98b507904fbb10a3d25245a6d57c702bc
imashwaaa <imashwaaa@gmail.com>
2026-09-06 16:19:41 +0530

    Problem 2 solved

---
+++ Basics/Practice_problems/sum_message.java
@@ -0,0 +1,11 @@
+package Practice_problems;
+
+public class sum_message {
+    public static void main(String[] args){
+        int a = 5, b = 7;
+        System.out.println("Sum of "+ a+" and "+ b+ " is "+(a+b));//concatination method
+        
+
+    }
+    
+}
