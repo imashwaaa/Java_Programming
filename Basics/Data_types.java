dbc7db334678dcc581b471f5c30f174a3d9a6536
imashwaaa <imashwaaa@gmail.com>
2026-09-06 16:41:33 +0530

    Add Data_types

---
+++ Basics/Data_types.java
@@ -0,0 +1,27 @@
+public class Data_types {
+    public static void main(String[] args){
+        byte b = 100;
+        short s = 200;
+        int i = 45988;
+        long l = 234444;
+        //these all come under integers category but they are classifed based on size as follows.
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        
+        //for the ones with decimal places float and double.
+        float f = 2.55F;
+        double d = 3.45454;
+        System.out.println(f);
+        System.out.println(d);
+
+        char c = 's'; // in character(char) you can store only a single alphabet. 
+        System.out.println(c);
+        boolean bool = false;//boolean data type just has two values-true and false
+        System.out.println(bool);
+
+    }
+    
+}
