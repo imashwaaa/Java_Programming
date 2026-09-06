10a8fc15d42b966c38569cac051bcb88d7c1ccf0
imashwaaa <imashwaaa@gmail.com>
2026-09-06 15:35:37 +0530

    First problem solved

---
+++ Basics/Practice_problems/sum_of_two_integers.java
@@ -0,0 +1,10 @@
+package Practice_problems;
+public class sum_of_two_integers {
+    public static void main(String[] args){
+        int a = 5, b = 7; // varibles created and values assigned
+        System.out.println(a+b);
+
+    }
+
+    
+}
