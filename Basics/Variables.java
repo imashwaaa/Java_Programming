86f890554541aa08bf6773617d7e4396638ac685
imashwaaa <imashwaaa@gmail.com>
2026-09-06 15:28:26 +0530

    Add Variables.java

---
+++ Basics/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+        public static void main(String[] args) {
+        
+        //variables
+        //syntax for variables is - dataType variableName = value
+        int num1;//variable declaration
+        num1 = 18; //variable initialisation
+        System.out.println("The value of num is " + num1);//prints the value of num using concatination
+        //the datatype used here is int, I'll learn more about it in upcoming sections
+        
+        
+
+    }
+}
