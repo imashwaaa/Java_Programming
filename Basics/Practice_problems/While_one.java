cad51ea53d504733aabb0172da5bbdaf8c09550c
imashwaaa <im.ashwaaa@gmail.com>
2026-09-26 12:25:34 +0530

    While loop problem one

---
+++ Basics/Practice_problems/While_one.java
@@ -0,0 +1,15 @@
+package Practice_problems;
+
+public class While_one {
+    public static void main(String[] args){
+        int i = 1; //start
+        int sum = 0; //empty var
+
+        while (i<=10){ //check
+            sum += i;//do
+            i++;//change
+        }
+        System.out.println(sum);
+    }
+    
+}
---
+++ Basics/While_loop.java
@@ -0,0 +1,11 @@
+public class While_loop {
+    public static void main(String[] args){
+        int i = 1; //START
+
+        while (i<=10){ //CHECK
+            System.out.println(i); //DO
+            i++; //CHANGE
+        }
+    }
+    
+}
